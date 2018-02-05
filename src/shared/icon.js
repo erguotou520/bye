a17b7c99d0378064d782d82d8af60c953420ce2d
erguotou <erguotou525@gmail.com>
2018-02-05 16:42:47 +0800

    更换图标

--- src/shared/icon.js
+++ src/shared/icon.js
@@ -1,8 +1,9 @@
 import { join } from 'path'
 import { nativeImage } from 'electron'
+import { isMac } from './env'
 
 function getImage (name, highlight = false) {
-  return nativeImage.createFromPath(join(__static, `${name}${highlight ? 'Highlight' : ''}.png`))
+  return nativeImage.createFromPath(join(__static, `${name}${isMac ? (highlight ? 'Highlight' : 'Template') : ''}.png`))
 }
 
 export let disabledTray
@@ -14,11 +15,11 @@ export let pacHighlightTray
 export let globalHighlightTray
 
 export function init () {
-  disabledTray = getImage('disabled', false)
+  disabledTray = getImage('disabled')
   enabledTray = getImage('enabled')
   pacTray = getImage('pac')
   globalTray = getImage('global')
-  enabledHighlightTray = getImage('enabled', true, true)
-  pacHighlightTray = getImage('pac', true, true)
-  globalHighlightTray = getImage('global', true, true)
+  enabledHighlightTray = getImage('enabled', true)
+  pacHighlightTray = getImage('pac', true)
+  globalHighlightTray = getImage('global', true)
 }
