c312f69b37a36b588fd80bbeca6339da5695c544
erguotou <erguotou525@gmail.com>
2018-02-08 16:50:34 +0800

    update notification

--- src/main/notification.js
+++ src/main/notification.js
@@ -1,7 +1,6 @@
 import { Notification } from 'electron'
 import { sendData } from './window'
 import { EVENT_APP_NOTIFY_MAIN } from '../shared/events'
-import { isWin } from '../shared/env'
 import { notificationIcon } from '../shared/icon'
 
 const isDesktopNotificationSupported = Notification.isSupported()
@@ -9,7 +8,7 @@ const isDesktopNotificationSupported = Notification.isSupported()
 export function showNotification (body, title = '通知', onClick) {
   if (isDesktopNotificationSupported) {
     const notification = new Notification({
-      title, body, silent: false, icon: isWin ? notificationIcon : undefined
+      title, body, silent: false, icon: notificationIcon
     })
     if (onClick) {
       notification.once('click', onClick)
