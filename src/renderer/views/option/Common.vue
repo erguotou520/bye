bc1da1a075a491dffd880dfaa0ee45059546e320
simon3000 <simon3000@163.com>
2018-04-15 02:11:37 +0800

    从Common中删除快捷键设置

--- src/renderer/views/option/Common.vue
+++ src/renderer/views/option/Common.vue
@@ -15,9 +15,6 @@
         <i-form-item class="flex-1" label="http代理">
           <i-checkbox v-model="form.httpProxyEnable" @on-change="update('httpProxyEnable')"/>
         </i-form-item>
-        <i-form-item class="flex-1" label="开启快捷键">
-          <i-checkbox v-model="form.shortcutEnable" @on-change="update('shortcutEnable')"/>
-        </i-form-item>
       </div>
       <div class="flex">
         <i-form-item class="flex-1" label="pac端口">
@@ -48,7 +45,6 @@ export default {
         localPort: appConfig.localPort,
         pacPort: appConfig.pacPort,
         httpProxyEnable: appConfig.httpProxyEnable,
-        shortcutEnable: appConfig.shortcutEnable,
         httpProxyPort: appConfig.httpProxyPort
       },
       rules: {
