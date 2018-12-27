fb1eddb83dca97a244b430cedb4df74d65a6298e
erguotou <erguotou525@gmail.com>
2018-12-27 14:48:41 +0800

    修复快捷键检测的bug

--- src/main/shortcut.js
+++ src/main/shortcut.js
@@ -65,9 +65,9 @@ app.on('ready', () => {
       // 注册，并返回注册失败的
       const failed = Object.keys(appConfig.globalShortcuts).filter(funcName => {
         if (appConfig.globalShortcuts[funcName].enable) {
-          return registerShortcut(funcName, appConfig.globalShortcuts[funcName].key)
+          return !registerShortcut(funcName, appConfig.globalShortcuts[funcName].key)
         }
-        return true
+        return false
       })
       if (failed.length) {
         showNotification(`检测到${failed.length}个全局快捷键注册失败，请在快捷键页面重新设置`, '错误', () => {
